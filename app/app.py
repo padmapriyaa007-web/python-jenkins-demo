f1be6c76c3b8ca5ece55e2d97cc586965352a473
padmapriyaa007-web <padmapriyaa007@gmail.com>
2026-06-08 00:08:44 +0530

    Update greeting message in home route

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello from DevOps this is priya Project 🚀"})
+    return jsonify({"message": "Hello from DevOps this is priyaa Project 🚀"})
 
 @app.route('/health')
 def health():
